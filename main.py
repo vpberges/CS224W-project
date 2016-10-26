5af3a8f721117fdb81f57ab794090879cda2e73a
K Truong <kl.truong@ucla.edu>
2016-10-26 16:25:21 -0700

    Added dictionary to look up edges between two nodes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from snap import *
+import collections
 import random
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,7 +10,7 @@ f = open('primary_training_part1.csv')
 f.readline()
 Graph.AddIntAttrE('Weight')
 Graph.AddIntAttrE('MonthId')
-
+EIds = collections.defaultdict(list)
 
 i=0
 for line in f:
@@ -22,17 +23,21 @@ for line in f:
 	#By convention, if A beat B, the arrow will be A <-- B
 	if WhiteScore == 1:
 		eId = Graph.AddEdge(BlackPlayer, WhitePlayer)
+                EIds[(BlackPlayer, WhitePlayer)].append(eId)
 		Graph.AddIntAttrDatE(eId, 1, 'Weight')
 		Graph.AddIntAttrDatE(eId, MonthID, 'MonthID')
 	if WhiteScore == 0:
 		eId = Graph.AddEdge(WhitePlayer, BlackPlayer)
+                EIds[(WhitePlayer, BlackPlayer)].append(eId)
 		Graph.AddIntAttrDatE(eId, 1, 'Weight')
 		Graph.AddIntAttrDatE(eId, MonthID, 'MonthID')
 	if WhiteScore == 0.5:
 		eId = Graph.AddEdge(WhitePlayer, BlackPlayer)
+                EIds[(WhitePlayer, BlackPlayer)].append(eId)
 		Graph.AddIntAttrDatE(eId, 0, 'Weight')
 		Graph.AddIntAttrDatE(eId, MonthID, 'MonthID')
 		eId = Graph.AddEdge(BlackPlayer, WhitePlayer)
+                EIds[(BlackPlayer, WhitePlayer)].append(eId)
 		Graph.AddIntAttrDatE(eId, 0, 'Weight')
 		Graph.AddIntAttrDatE(eId, MonthID, 'MonthID')
 
