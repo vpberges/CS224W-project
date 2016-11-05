489da9ea9cfcc2d74a2a4ab2bf95b355f1f6ea94
vpberges <vpberges@Vincent-Pierres-MacBook-Pro.local>
2016-11-05 15:12:19 -0700

    update

--- localProperties.py
+++ localProperties.py
@@ -2,15 +2,41 @@ from snap import *
 import collections
 import random
 import numpy as np
+import pandas as pd
 import utils
+
 
 Graph = TNEANet.New()
 Graph.AddIntAttrE('Weight')
 Graph.AddIntAttrE('MonthId')
 EIds = collections.defaultdict(list)
 
-Graph, EIds = utils.get_graph('../original_files/primary_training_part1', Graph, EIds)
-print Graph.GetNodes(), Graph.GetEdges()
+#Graph, EIds = utils.get_graph('training.csv', Graph, EIds)
+#Graph, EIds = utils.get_graph('../original_files/primary_training_part1', Graph, EIds)
 Graph, EIds = utils.get_graph('../original_files/primary_training_part2', Graph, EIds)
-print Graph.GetNodes(), Graph.GetEdges()
 
+f = open('training.csv')
+
+table = pd.DataFrame(columns = ['WhitePlayer', 'BlackPlayer', 'TrueWhiteScore', 'V', 'D', 'L'])
+
+i=0
+for line in f:
+	if 'PTID' in line:
+		continue
+	PTID, MonthID, WhitePlayer, BlackPlayer, WhiteScore, WhitePlayerPrev, BlackPlayerPrev = line.split(',')
+	MonthID, WhitePlayer, BlackPlayer, WhiteScore = int(MonthID), int(WhitePlayer), int(BlackPlayer), float(WhiteScore)
+	tmp_dict = {'WhitePlayer':WhitePlayer, 'BlackPlayer':BlackPlayer, 'TrueWhiteScore':WhiteScore}
+
+	#Number of V (victory):
+	for edge in EIds[(WhitePlayer,BlackPlayer)]:
+		print Graph.GetIntAttrDatE(edge, 'Weight')
+
+
+
+
+
+	table = table.append(pd.DataFrame(tmp_dict, index = [i]))
+	table.to_csv('localDataFrame.csv')
+	i+=1
+	if i>20:
+		break
--- utils.py
+++ utils.py
@@ -11,34 +11,34 @@ def get_graph(fileName, Graph, EIds ):
 	
 
 	for line in f:
-		try:
-			PTID, MonthID, WhitePlayer, BlackPlayer, WhiteScore, WhitePlayerPrev, BlackPlayerPrev = line.split(',')
-			MonthID, WhitePlayer, BlackPlayer, WhiteScore = int(MonthID), int(WhitePlayer), int(BlackPlayer), float(WhiteScore)
-			if not Graph.IsNode(WhitePlayer):
-				Graph.AddNode(WhitePlayer)
-			if not Graph.IsNode(BlackPlayer):
-				Graph.AddNode(BlackPlayer)
-			#By convention, if A beat B, the arrow will be A <-- B
-			if WhiteScore == 1:
-				eId = Graph.AddEdge(BlackPlayer, WhitePlayer)
-		                EIds[(BlackPlayer, WhitePlayer)].append(eId)
-				Graph.AddIntAttrDatE(eId, 1, 'Weight')
-				Graph.AddIntAttrDatE(eId, MonthID, 'MonthID')
-			if WhiteScore == 0:
-				eId = Graph.AddEdge(WhitePlayer, BlackPlayer)
-		                EIds[(WhitePlayer, BlackPlayer)].append(eId)
-				Graph.AddIntAttrDatE(eId, 1, 'Weight')
-				Graph.AddIntAttrDatE(eId, MonthID, 'MonthID')
-			if WhiteScore == 0.5:
-				eId = Graph.AddEdge(WhitePlayer, BlackPlayer)
-		                EIds[(WhitePlayer, BlackPlayer)].append(eId)
-				Graph.AddIntAttrDatE(eId, 0, 'Weight')
-				Graph.AddIntAttrDatE(eId, MonthID, 'MonthID')
-				eId = Graph.AddEdge(BlackPlayer, WhitePlayer)
-		                EIds[(BlackPlayer, WhitePlayer)].append(eId)
-				Graph.AddIntAttrDatE(eId, 0, 'Weight')
-				Graph.AddIntAttrDatE(eId, MonthID, 'MonthID')
-		except:
-			pass
+		if 'PTID' in line:
+			continue
+		PTID, MonthID, WhitePlayer, BlackPlayer, WhiteScore, WhitePlayerPrev, BlackPlayerPrev = line.split(',')
+		MonthID, WhitePlayer, BlackPlayer, WhiteScore = int(MonthID), int(WhitePlayer), int(BlackPlayer), float(WhiteScore)
+		if not Graph.IsNode(WhitePlayer):
+			Graph.AddNode(WhitePlayer)
+		if not Graph.IsNode(BlackPlayer):
+			Graph.AddNode(BlackPlayer)
+		#By convention, if A beat B, the arrow will be A <-- B
+		if WhiteScore == 1:
+			eId = Graph.AddEdge(BlackPlayer, WhitePlayer)
+	                EIds[(BlackPlayer, WhitePlayer)].append(eId)
+			Graph.AddIntAttrDatE(eId, 1, 'Weight')
+			Graph.AddIntAttrDatE(eId, MonthID, 'MonthID')
+		if WhiteScore == 0:
+			eId = Graph.AddEdge(WhitePlayer, BlackPlayer)
+	                EIds[(WhitePlayer, BlackPlayer)].append(eId)
+			Graph.AddIntAttrDatE(eId, 1, 'Weight')
+			Graph.AddIntAttrDatE(eId, MonthID, 'MonthID')
+		if WhiteScore == 0.5:
+			eId = Graph.AddEdge(WhitePlayer, BlackPlayer)
+	                EIds[(WhitePlayer, BlackPlayer)].append(eId)
+			Graph.AddIntAttrDatE(eId, 0, 'Weight')
+			Graph.AddIntAttrDatE(eId, MonthID, 'MonthID')
+			eId = Graph.AddEdge(BlackPlayer, WhitePlayer)
+	                EIds[(BlackPlayer, WhitePlayer)].append(eId)
+			Graph.AddIntAttrDatE(eId, 0, 'Weight')
+			Graph.AddIntAttrDatE(eId, MonthID, 'MonthID')
+
 	return Graph, EIds
 
