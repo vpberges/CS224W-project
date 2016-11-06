c6cf59fe28da1398a03107166ba99ca2aa781f07
vpberges <vpberges@DN0a22c144.SUNet>
2016-11-06 15:01:04 -0800

    worked on it a little

--- localML.py
+++ localML.py
@@ -10,12 +10,16 @@ from sklearn import linear_model, datasets
 from sklearn.ensemble import RandomForestClassifier
 
 
-table = pd.read_csv('localDataFrame.csv').fillna(0).drop('Unnamed: 0',1)
+table = pd.read_csv('localPropertiesSecondDataFrame.csv').fillna(0)#.drop('Unnamed: 0',1)
 predictors = list(table.columns.values)
 
 predictors.remove('WhitePlayer')
 predictors.remove('BlackPlayer')
 predictors.remove('TrueWhiteScore')
+try :
+	predictors.remove('PTID')
+except:
+	pass
 
 msk = np.random.rand(len(table)) < 0.8
 train = table[msk]
@@ -30,16 +34,19 @@ y_pred = logreg.predict(test[predictors].as_matrix())
 
 points = 0
 total = 0
+error = 0
 for pred, true in zip(y_pred ,[str(x) for x in list(test.TrueWhiteScore.values)] ):
+	error+= abs(float(pred) - float(true))
 	total += 1
 	if pred == true:
 		points+=1
+print 'LogisticRegression'
+print points *1.0 / total, error*1.0 / total
 
-print points *1.0 / total
 
 
 
-mod = RandomForestClassifier()
+mod = RandomForestClassifier(100)
 
 mod.fit(train[predictors].as_matrix(), [str(x) for x in list(train.TrueWhiteScore.values)])
 
@@ -48,11 +55,52 @@ y_pred = mod.predict(test[predictors].as_matrix())
 
 points = 0
 total = 0
+error = 0
 for pred, true in zip(y_pred ,[str(x) for x in list(test.TrueWhiteScore.values)] ):
+	error+= abs(float(pred) - float(true))
 	total += 1
 	if pred == true:
 		points+=1
 
-print points *1.0 / total
+print 'RandomForestClassifier'
+print points *1.0 / total, error*1.0 / total
+
+
+print 'Linear Lasso'
+mod = linear_model.Lasso(0.4)
+
+mod.fit(train[predictors].as_matrix(), [float(x) for x in list(train.TrueWhiteScore.values)])
+
+y_pred = mod.predict(test[predictors].as_matrix())
+
+points = 0
+total = 0
+error = 0
+for pred, true in zip(y_pred ,[str(x) for x in list(test.TrueWhiteScore.values)] ):
+	error+= abs(float(pred) - float(true))
+	total += 1
+	if pred == true:
+		points+=1
+
+print points *1.0 / total, error*1.0 / total
+
+
+print 'Random'
+y_pred = [str(x) for x in list(test.TrueWhiteScore.values)]
+random.shuffle(y_pred)
+
+points = 0
+total = 0
+error = 0
+for pred, true in zip(y_pred ,[str(x) for x in list(test.TrueWhiteScore.values)] ):
+	error+= abs(float(pred) - float(true))
+	total += 1
+	if pred == true:
+		points+=1
+
+print points *1.0 / total, error*1.0 / total
 
 print total
+
+
+
--- localProperties.py
+++ localProperties.py
@@ -13,7 +13,7 @@ Graph.AddIntAttrE('MonthId')
 EIds = collections.defaultdict(list)
 
 #Graph, EIds = utils.get_graph('training.csv', Graph, EIds)
-Graph, EIds = utils.get_graph('../original_files/primary_training_part1', Graph, EIds)
+#Graph, EIds = utils.get_graph('../original_files/primary_training_part1', Graph, EIds)
 Graph, EIds = utils.get_graph('../original_files/primary_training_part2', Graph, EIds)
 #Graph, EIds = utils.get_graph('very_tiny', Graph, EIds)
 
@@ -40,19 +40,21 @@ def GetLinksOrder(A, B, order = 0, visited = []):
 		for edge in EIds[(B, A)]:
 			if Graph.GetIntAttrDatE(edge, 'Weight') == 1 :
 				n_victory+=1
-		return {'V':n_victory, 'D':n_draw, 'L':n_lost}, visited
+		return {'V':n_victory, 'D':n_draw, 'L':n_lost}
 		# return {'V':[str(B)], 'D':[str(B)], 'L':[str(B)]}, visited
 	dict_result = {}
-	list_intermediates = set([ N for N in Graph.GetNI(A).GetInEdges()]) | set([ N for N in Graph.GetNI(A).GetOutEdges()])
+	list_intermediates = sorted([ N for N in Graph.GetNI(A).GetInEdges()] + [ N for N in Graph.GetNI(A).GetOutEdges()])
+	last_element_list = None
 	for N_inter in list_intermediates:
+		if N_inter == last_element_list :
+			continue
+		last_element_list = N_inter
 		if N_inter in visited:
 			#print N_inter, visited
 			continue
 			#1+1
 		# print 'order : ', order
-		old_visited = copy.deepcopy(visited)
-		inter_dict, visited = GetLinksOrder(N_inter, B, order - 1, visited +[N_inter])
-		visited = old_visited
+		inter_dict = GetLinksOrder(N_inter, B, order - 1, visited +[N_inter])
 		# print inter_dict, N_inter, B , order - 1
 		n_victory = 0
 		n_draw = 0
@@ -71,7 +73,7 @@ def GetLinksOrder(A, B, order = 0, visited = []):
 			dict_result['D'+key] = dict_result.get('D'+key, 0) + inter_dict.get(key, 0) * n_draw    
 			dict_result['L'+key] = dict_result.get('L'+key, 0) + inter_dict.get(key, 0) * n_lost
 
-	return dict_result, visited
+	return dict_result
 
 
 
@@ -79,9 +81,13 @@ def GetLinksOrder(A, B, order = 0, visited = []):
 
 
 
-f = open('training.csv')
 
-table = pd.DataFrame(columns = ['WhitePlayer', 'BlackPlayer', 'TrueWhiteScore', 'V', 'D', 'L'])
+f = open('training.csv', 'r')
+#f = open('very_tiny.csv')
+output = open('localPropertiesSecondDataFrame.csv','w')
+# output.write('DVL,DL,White_D,VDV,DVD,DD,DDL,White_L,Black_L,LLV,VDD,White_V,DV,DVV,VDL,LD,VLL,LDD,VVV,LL,Black_D,VLD,LDL,LV,DDV,VVL,LDV,LLL,VVD,VLV,LVV,VD,D,DLD,White_PRank,VL,BlackPlayer,L,LVD,WhitePlayer,DLV,VV,V,LVL,DLL,DDD,Black_PRank,TrueWhiteScore,LLD,Black_V\n')
+
+#table = pd.DataFrame(columns = ['WhitePlayer', 'BlackPlayer', 'TrueWhiteScore', 'V', 'D', 'L'])
 
 PRankH = TIntFltH()
 GetPageRank(Graph, PRankH)
@@ -94,22 +100,39 @@ i=0
 for line in f:
 	if 'PTID' in line:
 		continue
+
 	PTID, MonthID, WhitePlayer, BlackPlayer, WhiteScore, WhitePlayerPrev, BlackPlayerPrev = line.split(',')
 	MonthID, WhitePlayer, BlackPlayer, WhiteScore = int(MonthID), int(WhitePlayer), int(BlackPlayer), float(WhiteScore)
-	tmp_dict = {'WhitePlayer':WhitePlayer, 'BlackPlayer':BlackPlayer, 'TrueWhiteScore':WhiteScore}
+	tmp_dict = {'DVL': 0, 'DL': 0, 'White_D': 0, 'VDV': 0, 'DVD': 0, 'DD': 0, 'DDL': 0, 'White_L': 0, 'Black_L': 0, 'LLV': 0,\
+	 'VDD': 0, 'White_V': 0, 'DV': 0, 'DVV': 0, 'VDL': 0, 'LD': 0, 'VLL': 0, 'LDD': 0, 'VVV': 0, 'LL': 0, 'Black_D': 0, 'VLD': 0, \
+	 'LDL': 0, 'LV': 0, 'DDV': 0, 'VVL': 0, 'LDV': 0, 'LLL': 0, 'VVD': 0, 'VLV': 0, 'LVV': 0, 'VD': 0, 'D': 0, 'DLD': 0, \
+	 'White_PRank': 0, 'VL': 0, 'BlackPlayer': BlackPlayer, 'L': 0, 'LVD': 0, 'WhitePlayer': WhitePlayer, 'DLV': 0, 'VV': 0, 'V': 0, 'LVL': 0, \
+	 'DLL': 0, 'DDD': 0, 'Black_PRank': 0, 'TrueWhiteScore': WhiteScore, 'LLD': 0, 'Black_V': 0}
 
 
 	if Graph.IsNode(WhitePlayer) and Graph.IsNode(BlackPlayer):
 		for deg in range(max_intermediate+1):
-			tmp_dict.update(GetLinksOrder(WhitePlayer, BlackPlayer, order = deg, visited = [WhitePlayer,BlackPlayer])[0])
+			try:
+				tmp_dict.update(GetLinksOrder(WhitePlayer, BlackPlayer, order = deg, visited = [WhitePlayer,BlackPlayer]))
+			except:
+				raise
+
+
+
+
+
 
 	#Can add other predictors including a pageRank
 	if Graph.IsNode(WhitePlayer):
-		list_intermediates = set([ N for N in Graph.GetNI(WhitePlayer).GetInEdges()]) | set([ N for N in Graph.GetNI(WhitePlayer).GetOutEdges()])
 		n_victory = 0
 		n_draw = 0
 		n_lost = 0
+		list_intermediates = sorted([ N for N in Graph.GetNI(WhitePlayer).GetInEdges()] + [ N for N in Graph.GetNI(WhitePlayer).GetOutEdges()])
+		last_element_list = None
 		for N_inter in list_intermediates:
+			if N_inter == last_element_list :
+				continue
+			last_element_list = N_inter
 			for edge in EIds[(WhitePlayer, N_inter)]:
 				if Graph.GetIntAttrDatE(edge, 'Weight') == 1 :
 					n_lost+=1
@@ -123,11 +146,15 @@ for line in f:
 		tmp_dict['White_L'] = n_lost
 		tmp_dict['White_PRank'] = PRankH[WhitePlayer]
 	if Graph.IsNode(BlackPlayer):
-		list_intermediates = set([ N for N in Graph.GetNI(BlackPlayer).GetInEdges()]) | set([ N for N in Graph.GetNI(BlackPlayer).GetOutEdges()])
 		n_victory = 0
 		n_draw = 0
 		n_lost = 0
+		list_intermediates = sorted([ N for N in Graph.GetNI(BlackPlayer).GetInEdges()] + [ N for N in Graph.GetNI(BlackPlayer).GetOutEdges()])
+		last_element_list = None
 		for N_inter in list_intermediates:
+			if N_inter == last_element_list :
+				continue
+			last_element_list = N_inter
 			for edge in EIds[(BlackPlayer, N_inter)]:
 				if Graph.GetIntAttrDatE(edge, 'Weight') == 1 :
 					n_lost+=1
@@ -142,18 +169,22 @@ for line in f:
 		tmp_dict['Black_PRank'] = PRankH[BlackPlayer]
 
 
+	if i==0:
+		output.write('PTID,'+','.join([str(x) for x in tmp_dict.keys()]) + '\n')
+	output.write(str(PTID) +','+ ','.join([str(x) for x in tmp_dict.values()]) + '\n')
 
 
+	#output.write(','.join(tmp_dict.values()))
 
 	i+=1
 
 	# if i>20:
 	# 	break
-	table = table.append(pd.DataFrame(tmp_dict, index = [i]))
-	if i % 500 == 0 :
-		table.to_csv('localDataFrame.csv')
-		print i
+	#table = table.append(pd.DataFrame(tmp_dict, index = [i]))
+	#if i % 500 == 0 :
+		#table.to_csv('localDataFrame.csv')
+		#print i
 
-table = table.append(pd.DataFrame(tmp_dict, index = [i]))
-table.to_csv('localDataFrame.csv')
+#table = table.append(pd.DataFrame(tmp_dict, index = [i]))
+#table.to_csv('localDataFrame.csv')
 
