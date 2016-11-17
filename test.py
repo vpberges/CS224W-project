0bd5779ae205409225d60f82834ece5de89dab09
K Truong <kl.truong@ucla.edu>
2016-11-17 14:00:39 -0800

    Fix confusion matrix and balanced accuracy bugs

--- test.py
+++ test.py
@@ -62,13 +62,13 @@ def accuracy(test, predicted, **kwargs):
 	cnf_matrix = confusion_matrix(map(str,y_true), map(str,y_hat))
 	print '############# Method : ', predicted[11:-4], ' ################' 
 	print 'Mean Absolute Difference Error: ', sum(abs(y_true-y_hat))/len(y_true-y_hat) 
-	print 'Exact Accyracy: ', sum(abs(y_true-y_hat) == 0)*1.0/len(y_true-y_hat) 
-	print 'Balanced Accuracy: ', np.trace(cnf_matrix*1.0/np.sum(cnf_matrix, axis = 0))/3.0
+	print 'Exact Accuracy: ', sum(abs(y_true-y_hat) == 0)*1.0/len(y_true-y_hat) 
+	print 'Balanced Accuracy: ', np.trace(cnf_matrix*1.0/np.sum(cnf_matrix, axis = 1))/3.0
 	if(plot):
 		np.set_printoptions(precision=2)
 		# Plot non-normalized confusion matrix
 		plt.figure()
-		plot_confusion_matrix(cnf_matrix, classes=['win', 'draw', 'loss'], title='Confusion matrix ' + predicted[11:-4])
+		plot_confusion_matrix(cnf_matrix, classes=['loss', 'draw', 'win'], normalize=False, title='Confusion matrix ' + predicted[11:-4])
 		plt.show()
 
 	return y_true, y_hat, cnf_matrix
